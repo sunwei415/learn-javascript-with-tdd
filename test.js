0a682f64535ca96cbf3db0edf60808c524622822
Wei Sun <sunwei415@gmail.com>
2016-11-30 15:04:17 +0800

    done with sum function.

--- test.js
+++ test.js
@@ -1,4 +1,31 @@
 var assert = require('assert');
+
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -60,7 +87,7 @@ describe('Array', function() {
                 setTimeout(function () {
                     console.log(2);
                     done();
-                }, 1000);
+                }, 100);
 
                 setTimeout(function () {
                     console.log(3);
@@ -71,5 +98,60 @@ describe('Array', function() {
             })();
         });
 
+    });
+
+    describe('Array.prototype.slice', function () {
+        it('[1,2,3] slice for second and third', function () {
+            var myArray = [1,2,3];
+            var sliced = myArray.slice(1,myArray.length);
+            console.log("sliced:", sliced);
+            assert(sliced.equals([2,3]));
+        });
+
+        it('equals', function () {
+            assert([2,3].equals([2,3]));
+        });
+    });
+
+    describe('sum', function () {
+
+        var sum = function () {
+            var argumentLength = arguments.length;
+            console.log(argumentLength);
+
+            var first = arguments[0];
+            if (argumentLength > 2) {
+
+                console.log("typeof arguments");
+                console.log(typeof arguments);
+
+                return first + sum.apply(this, Array.prototype.slice.call(arguments, 1));
+            }
+            else if (argumentLength == 2) {
+                return first + arguments[1];
+            }
+            else if (argumentLength == 1){
+                return function (x) {
+                    return first + x;
+                }
+            }
+        };
+
+        it('sum(2,3)', function () {
+            assert(sum(2,3) == 5);
+        });
+
+        it('sum(2)(3)', function () {
+            assert(sum(2)(3) == 5);
+        });
+
+        it('sum(2,3,4)', function () {
+            assert(sum(2,3,4) == 9);
+        });
+
+        it('sum(2)(3)(4)', function () {
+            //not possible to make it work
+            // assert(sum(2)(3)(4) == 9);
+        });
     });
 });
